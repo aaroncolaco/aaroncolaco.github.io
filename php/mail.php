98b600c7b501f578f68e90617c1bb9af3a2fea7c
aarongoa <taipan93@gmail.com>
2016-02-10 20:22:16 +0530

    Trying to fix form

--- php/mail.php
+++ php/mail.php
@@ -3,33 +3,31 @@
 	ini_set('display_errors',1);
 
 	// Check for empty fields
-	/*if(empty($_POST['name'])  		||
+	if(empty($_POST['name'])  		||
 		empty($_POST['email']) 		||
-		empty($_POST['message'])		||
+		empty($_POST['message'])	||
 		!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-	{
-	echo "No arguments Provided!";
-	return false;
-	}*/
+			{
+				echo "No arguments Provided!";
+				return false;
+			}
 
-	$name = "John";
-	$email = "taipan93@gmail.com";
-	$message = "Hello World";
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
 
 	// set here
 	$subject = "Website Contact Form:  $name";
-	$to = "taipan93@gmail.com"; /* add email address here*/
+	$to = "aaroncolaco.work@gmail.com"; /* add email address here*/
 
-	$headers = "From: noreply@aaroncolaco.com\n";
-	// $headers .= "Content-type: text/plain\r\n";		/* needs to be text/plain and NOT text/html so that the newline characters work. Otherwise received email is in one line */
+	$headers = "From: noreply@aaroncolaco.com\r\n";
+	$headers .= "Content-type: text/plain\r\n";		/* needs to be text/plain and NOT text/html so that the newline characters work. Otherwise received email is in one line */
 
 	$headers .= "Reply-To: $email";
 	
 	// send the email
 	mail($to, $subject, $message, $headers);
 
-	return true;
-	
 	#redirect
-	//header( "Location: http://aaroncolaco.com/#contact" );	//this line is important. remember to change domain if you use the script on a new domain. otherwise it won't return and carry out remaining operations after sending the email
+	header( "Location: http://aaroncolaco.com/#contact" );	//this line is important. remember to change domain if you use the script on a new domain. otherwise it won't return and carry out remaining operations after sending the email
 ?>
